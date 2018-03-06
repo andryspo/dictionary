bd425d6d9c48516c86edc60112f26e0d8fda88e3
Andriy <andryi2010@ukr.net>
2018-03-06 23:01:53 +0200

    first commit

---
+++ src/main/java/com/andryspo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.andryspo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+
+        System.out.printf("sss");
+        return "index";
+    }
+
+}
